1788d2eeea9b5362242e1191daac6c121969a309
Andrey Egoshin <andrey.egoshin@ispringsolutions.com>
2021-01-09 19:56:50 +0300

    Поправил запуск образа

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #exec xhost +local:meter
-exec docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v "$HOME"/projects/meter/app:/usr/local/app --device /dev/video0:/dev/video0 meter
+exec docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v "$HOME"/projects/meter/app:/usr/local/app --device /dev/video0:/dev/video0 andrey540/meter
